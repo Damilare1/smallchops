e0d5e961d53834bd16e22c94a4b67ae6564fb622
Damilare Adedoyin <damilare182@gmail.com>
2019-06-12 13:47:20 +0100

    thirdChange

--- src/firstPage.js
+++ src/firstPage.js
@@ -1,12 +1,12 @@
-import React, { Component, Fragment } from 'react';
-import axios from 'axios'
+import React, { Component, Fragment } from "react";
+import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://api.paystack.co/",
   timeout: 5000,
   headers: {
-    Authorization: 'Bearer sk_test_cf0616be6156226583bf8ad620f490ae5fa27e5d',
-  //  'Content-Type': 'application/json'
+    Authorization: "Bearer sk_test_cf0616be6156226583bf8ad620f490ae5fa27e5d"
+    //  'Content-Type': 'application/json'
   }
 });
 
@@ -15,40 +15,43 @@ export default class FirstPage extends Component {
     super(props);
 
     this.state = {
-      listBank:[],
+      listBank: [],
       listCustomers: [],
-      account_number: '',
-      account_name: '',
-    }
+      account_number: "",
+      account_name: "",
+      bank_code: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
-  //  this.handleSubmit = this.handleSubmit.bind(this);
+    this.verify = this.verify.bind(this);
+    //  this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
 
- 
   componentDidMount() {
-    
-    axiosInstance.get( "bank")
-      .then(res => {
+    axiosInstance.get("bank").then(
+      res => {
         const listBanks = res.data.data;
-        this.setState( {listBank: listBanks} );
+        this.setState({ listBank: listBanks });
       },
-      (error) => {
-        console.log(error.response.status)
-      })
+      error => {
+        console.log(error.response.status);
+      }
+    );
   }
 
-  verify(event){
-    axiosInstance.get( "bank/resolve")
-    .then(res => {
-      console.log(res)
-      const listBanks = res.data.data;
-      this.setState( {listBank: listBanks} );
-    },
-    (error) => {
-      console.log(error.response.status)
-    })
+  verify(event) {
+    const { account_number, bank_code } = this.state;
+
+    axiosInstance.get("bank/resolve", { account_number, bank_code }).then(
+      res => {
+        console.log(res);
+        const accountDetails = res.data.data;
+        this.setState({ account_name: accountDetails.account_name });
+      },
+      error => {
+        console.log(error.response.status);
+      }
+    );
   }
 
   handleChange(event) {
@@ -61,45 +64,45 @@ export default class FirstPage extends Component {
       account_number,
       listBank,
       listCustomers,
-      account_name,
+      account_name
     } = this.state;
     return (
-      <form>
-      <div>
-        <label className="">
-          Account Number
-              <span className="">*</span>
-        </label>
-        <input
-          type="text"
-          name="account_number"
-          required
-          placeholder="Enter Account Number"
-        />
-      </div>
+      <form onSubmit={this.verify}>
+        <div>
+          <label className="">
+            Account Number
+            <span className="">*</span>
+          </label>
+          <input
+            type="text"
+            name="account_number"
+            required
+            placeholder="Enter Account Number"
+          />
+        </div>
 
-      <div>
-        <select
-              className="form-control"
-              required
-              name="type"
-              onChange={this.handleChange}
-            >
-              <option value="">
-                Select Bank
+        <div>
+          <select
+            className="form-control"
+            required
+            name="type"
+            onChange={this.handleChange}
+          >
+            <option value="">Select Bank</option>
+
+            {listBank.map(bank => (
+              <option key={bank.name} value={bank.code}>
+                {bank.name}
               </option>
+            ))}
 
-              {listBank.map(bank => (
-                <option key={bank.name} value={bank.code}>
-                  {bank.name}
-                </option>
-              ))}
-
-              <option value="">
-                Others
-              </option>
-            </select>
-      </div>
+            <option value="">Others</option>
+          </select>
+        </div>
+        <span>{account_name}</span>
+        <button className="btn btn-primary bd-0" type="submit">
+          submit
+          </button>
       </form>
     );
   }
